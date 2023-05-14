e8eeaaae01fc76979d5a3ee4b114187458170aea
Douglas Eduardo <doug.edgates@gmail.com>
2023-05-14 10:29:04 -0300

    hotfix: Adicionados alguns comentários.

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-﻿$(document).ready(function() {
+﻿$(document).ready(function() { // Slide Down do "NOVA IMAGEM"
     $('header button').click(function() {
         $('form').slideDown();
     })
 
-    $('#botao-cancelar').click(function() {
+    $('#botao-cancelar').click(function() { // Slide Up do "CANCELAR"
         $('form').slideUp();
     })
 
-    $('form').on('submit', function(evento) {
+    $('form').on('submit', function(evento) { // Evento do 'submit'
         console.log("submit");
         evento.preventDefault();
     })
