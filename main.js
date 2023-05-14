7382656c7a0b0925197f27b4c34b11657b8a39e6
Douglas Eduardo <doug.edgates@gmail.com>
2023-05-14 10:25:15 -0300

    first commit: Construído o HTML, o CSS. No JavaScript foram feitos o 'Slide Down' ao clicar em 'NOVA IMAGEM +', o 'Slide Up' ao clicar em 'CANCELAR' e o evento do 'submit'.

---
+++ main.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function() {
+    $('header button').click(function() {
+        $('form').slideDown();
+    })
+
+    $('#botao-cancelar').click(function() {
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(evento) {
+        console.log("submit");
+        evento.preventDefault();
+    })
+})
