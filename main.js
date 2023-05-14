47afd5bfd0913c7aeb636f34795c00d87da38806
Douglas Eduardo <doug.edgates@gmail.com>
2023-05-14 11:02:48 -0300

    Adicionada a função para inserir uma nova imagem

--- main.js
+++ main.js
@@ -8,7 +8,19 @@
     })
 
     $('form').on('submit', function(evento) { // Evento do 'submit'
-        console.log("submit");
         evento.preventDefault();
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+        const novoItem = $('<li style="display: none"></li>');
+        $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem); // Ele vai inserir elemento entre os <li></li>
+        $(`
+            <div class="overlay-imagem-link">
+                <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
+                    Ver imagem em tamanho real
+                </a>
+            </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+        $('#endereco-imagem-nova').val('');
     })
 })
